db4ccf3690bb266a815d5831651d7d5763b7894b
helvetios16 <smendozafernandez437@gmail.com>
2025-10-30 02:38:16 -0500

    Agregando UI para la vista de la transaccion actual

---
+++ Assets/Scripts/UIContadorController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIContadorController : MonoBehaviour
+{
+    public TMP_Text textoTransaccion;
+
+    void Start()
+    {
+        if (textoTransaccion == null)
+        {
+            Debug.LogError("UIContadorController: textoTransaccion no está asignado en el Inspector");
+            return;
+        }
+        textoTransaccion.text = "Transacción: ";
+    }
+
+
+    public void ActualizarTransaccion(int monto)
+    {
+        textoTransaccion.text = "Transacción: $" + monto.ToString();
+    }
+}
+
